270fd87070c2f9a94378ef3e876cb6b34e8ee2ce
Time of Flight LLC <noreply@timeofflightllc.com>
2026-09-08 02:37:41 +0000

    Match the About signature background to the page navy.

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -39,7 +39,7 @@ function About() {
         <SiteCopyBody body={page.body} />
         <figure className="mt-4 max-w-sm">
           <img
-            src="/brand/cain-signature.jpg"
+            src="/brand/cain-signature.png"
             alt="Cain"
             className="h-20 w-auto sm:h-24"
           />
